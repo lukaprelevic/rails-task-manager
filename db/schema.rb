aeaa5b91f37b89a93c572e129c6a34961274a823
Luka Prelevic <lukaprelevic@gmail.com>
2017-11-07 11:47:50 +0000

    Added default to tasks completed boolean false

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107111626) do
+ActiveRecord::Schema.define(version: 20171107114521) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.text     "details"
-    t.boolean  "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "completed",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
